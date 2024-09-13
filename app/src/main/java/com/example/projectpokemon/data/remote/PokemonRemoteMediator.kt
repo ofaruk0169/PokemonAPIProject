1e4dab35f0ac4908d16cc0998dc013c7d70e9250
Omare Faruk <ofaruk0169@gmail.com>
2024-09-13 21:57:21 +0100

    code done, logic in my brain isn't there do it tomorrow

--- app/src/main/java/com/example/projectpokemon/data/remote/PokemonRemoteMediator.kt
+++ app/src/main/java/com/example/projectpokemon/data/remote/PokemonRemoteMediator.kt
@@ -4,9 +4,11 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import coil.network.HttpException
 import com.example.projectpokemon.data.local.PokemonDatabase
 import com.example.projectpokemon.data.local.PokemonEntity
+import com.example.projectpokemon.data.mappers.toPokemonEntity
 import java.io.IOException
 
 @OptIn(ExperimentalPagingApi::class)
@@ -20,6 +22,38 @@ class PokemonRemoteMediator (
         state: PagingState<Int, PokemonEntity>
     ): MediatorResult {
         return try {
+            val loadKey = when(loadType) {
+                LoadType.REFRESH -> 1
+                LoadType.PREPEND -> return MediatorResult.Success(
+                    endOfPaginationReached = true
+                )
+                LoadType.APPEND -> {
+                    val lastItem = state.lastItemOrNull()
+                    if(lastItem == null) {
+                        1
+                    } else {
+                        (lastItem.id / state.config.pageSize) + 1
+                    }
+                }
+            }
+
+            val pokemons = pokemonApi.getPokemons(
+                page = loadKey,
+                pageCount = state.config.pageSize
+            )
+
+            pokemonDb.withTransaction {
+                if(loadType == LoadType.REFRESH) {
+                    pokemonDb.dao.clearAll()
+                }
+                val pokemonEntities = pokemons.map { it.toPokemonEntity() }
+                pokemonDb.dao.upsertAll(pokemonEntities)
+            }
+
+            MediatorResult.Success(
+                endOfPaginationReached = pokemons.isEmpty()
+            )
+
 
         } catch(e: IOException) {
             MediatorResult.Error(e)
