2e8728e74d156b1771ba9f7b23d6fb0f55e969dd
Omare Faruk <ofaruk0169@gmail.com>
2024-10-31 00:10:06 +0000

    added

--- app/src/main/java/com/example/projectpokemon/data/remote/PokemonDto.kt
+++ app/src/main/java/com/example/projectpokemon/data/remote/PokemonDto.kt
@@ -1,12 +1,11 @@
 package com.example.projectpokemon.data.remote
 
 data class PokemonDto(
-/*    val id: Int,
-    val name: String,
+    val id: Int,
     val types: String, //"tagline" in Phillip video
     val abilities: String, //"description" in Phillip video
     val height: String, //"first_brewed" in Phillip video
-    val spriteUrl: String? //"image_url" in Phillip video*/
+    val spriteUrl: String?, //"image_url" in Phillip video
 
 
     val name: String,
