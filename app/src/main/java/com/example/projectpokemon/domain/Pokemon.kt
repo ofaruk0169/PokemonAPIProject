35c3b8ef9edab03c2f712c5ffe008b1fb6b527c6
Omare Faruk <ofaruk0169@gmail.com>
2025-01-06 21:13:06 +0000

    got rid of fluff code

--- app/src/main/java/com/example/projectpokemon/domain/Pokemon.kt
+++ app/src/main/java/com/example/projectpokemon/domain/Pokemon.kt
@@ -3,8 +3,8 @@ package com.example.projectpokemon.domain
 data class Pokemon(
     val id: Int,
     val name: String,
-    val types: String, //"tagline" in Phillip video
-    val abilities: String, //"description" in Phillip video
-    val height: String, //"first_brewed" in Phillip video
-    val spriteUrl: String? //"image_url" in Phillip video
+    val types: String,
+    val abilities: String,
+    val height: String,
+    val spriteUrl: String?
 )
