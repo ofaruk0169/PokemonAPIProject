df7a351dcf664a5a1d6608de69a863bb51bca062
Omare Faruk <ofaruk0169@gmail.com>
2025-01-26 13:06:57 +0000

    commented out test code

--- app/src/test/java/com/example/projectpokemon/data/remote/PokemonRemoteMediatorTest.kt
+++ app/src/test/java/com/example/projectpokemon/data/remote/PokemonRemoteMediatorTest.kt
@@ -7,11 +7,11 @@ import org.junit.Before
 class PokemonRemoteMediatorTest {
     private lateinit var pokemonRemoteMediator: PokemonRemoteMediator
 
-    @Before
+/*    @Before
     fun setUp() {
         pokemonRemoteMediator = PokemonRemoteMediator(
             //here we need to pass both the database and the API, did a litttle bit here so that's a start.
 
         )
-    }
+    }*/
 }
